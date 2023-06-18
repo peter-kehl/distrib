116bfef52e362a97d335b4673239ab9e1892c36b
Peter Kehl <peter.kehl@gmail.com>
2023-06-18 00:26:51 -0700

    Interoperable and chainable - ergonomic.

--- src/lib.rs
+++ src/lib.rs
@@ -15,20 +15,20 @@ impl <T> TrackS<T> {
 }*/
 // ---
 
+/// Estimated plan. It collects expected storage, bandwidth + latency, and computation costs and
+/// constraints.
 pub trait Plan: Send + Sized {}
+/// Executed plan. It collects actual storage, bandwidth + latency, and computation costs.
 pub trait Real: Send + Sized {}
-//pub trait Data: Send + Sized {}
-//impl Data for u8 {}
-//...
 
-pub trait TrackT<P: Plan, R: Real, D: Send + Sized> {
+/*pub trait TrackT<P: Plan, R: Real, D: Send + Sized> {
     fn plan(&self) -> () { // return a closure?
     }
 
     fn process_t(&self) -> D {
         loop {}
     }
-}
+}*/
 
 /// [Send] is required, so that async runtimes can move the result across thread.
 ///
@@ -53,7 +53,7 @@ pub trait PlanHolder<P: Plan>: Send + Sized {
         unreachable!()
     }
     /// Whether we are in the `Plan` mode (rather than `Process` mode).
-    fn is_plan(&self) -> bool {
+    fn being_planned(&self) -> bool {
         unreachable!()
     }
 }
@@ -105,22 +105,22 @@ const _: () = {
     }
     assert!(core::mem::size_of::<PRDHS>() == 0);
 };
-pub struct Prd<P: Plan, R: Real, PRDHS: PlanRealDataHolders<P, R>, D: Send + Sized> {
+pub struct PrdInner<P: Plan, R: Real, PRDHS: PlanRealDataHolders<P, R>, D: Send + Sized> {
     _p: PhantomData<P>,
     _r: PhantomData<R>,
     _prdh: PhantomData<PRDHS>,
     _d: PhantomData<D>,
 
-    plan_holder: PRDHS::PLAN,
-    real_holder: PRDHS::REAL,
-    data_holder: PRDHS::DATA<D>,
+    pub plan_holder: PRDHS::PLAN,
+    pub real_holder: PRDHS::REAL,
+    pub data_holder: PRDHS::DATA<D>,
 }
 unsafe impl<P: Plan, R: Real, PRDHS: PlanRealDataHolders<P, R>, D: Send + Sized> Send
-    for Prd<P, R, PRDHS, D>
+    for PrdInner<P, R, PRDHS, D>
 {
 }
 
-impl<P: Plan, R: Real, PRDHS: PlanRealDataHolders<P, R>, D: Send + Sized> Prd<P, R, PRDHS, D> {
+impl<P: Plan, R: Real, PRDHS: PlanRealDataHolders<P, R>, D: Send + Sized> PrdInner<P, R, PRDHS, D> {
     pub fn plan_data_moved(self) -> (P, D) {
         PRDHS::plan_data_moved((self.plan_holder, self.data_holder))
     }
@@ -138,7 +138,107 @@ impl<P: Plan, R: Real, PRDHS: PlanRealDataHolders<P, R>, D: Send + Sized> Prd<P,
     }
     /// Whether we are in the `Plan` mode (rather than `Process` mode).
     pub fn is_plan(&self) -> bool {
-        self.plan_holder.is_plan()
+        self.plan_holder.being_planned()
+    }
+}
+
+pub trait PrdTypes: Send + Sized {
+    type P: Plan;
+    type R: Real;
+    type PRDHS: PlanRealDataHolders<Self::P, Self::R>;
+}
+
+/*pub trait MoveInnerExact<PTS: PrdTypes, D: Send + Sized> : Sized {
+    fn move_inner(self) -> PrdInner<PTS::P, PTS::R, PTS::PRDHS, D>;
+}
+
+pub trait MoveInner<INNER: Send + Sized> : Sized {
+    fn move_inner(self) -> INNER;
+}*/
+
+/// Generate a user space struct that contains one item of type [PrdInner]. This macro, invoked from
+/// user space, accepts optional params:
+/// - `struct_vis` indicating visibility of the struct (otherwise it's private),
+/// - `inner_vis` indicating visibility of `inner` (otherwise it's private),
+/// - `method_vis` indicating visibility of methods (otherwise they are private),
+/// - `struct_name` (if other than `Prd`).
+#[macro_export]
+macro_rules! generate_prd_struct {
+    // See also https://veykril.github.io/tlborm/decl-macros/minutiae/fragment-specifiers.html#vis.
+    ($struct_vis:vis, $inner_vis:vis, $method_vis:vis, $struct_name:ident) => {
+        /// A struct that carries [distrib::PrdInner].
+        ///
+        /// This struct exists in user space, so that the user can implement methods on it. That
+        /// allows chaining method calls - more ergonomic.
+        #[repr(transparent)]
+        $struct_vis struct $struct_name<PTS: $crate::PrdTypes, D: ::core::marker::Send + ::core::marker::Sized> {
+            $inner_vis inner: $crate::PrdInner<PTS::P, PTS::R, PTS::PRDHS, D>,
+        }
+        unsafe impl<PTS: $crate::PrdTypes, D: ::core::marker::Send + ::core::marker::Sized> ::core::marker::Send for $struct_name<PTS, D> {}
+
+        impl<PTS: $crate::PrdTypes, D: ::core::marker::Send + ::core::marker::Sized> $struct_name<PTS, D> {
+            $method_vis fn new(inner: $crate::PrdInner<PTS::P, PTS::R, PTS::PRDHS, D>) -> Self {
+                Self { inner }
+            }
+
+            $method_vis fn inner(self) -> $crate::PrdInner<PTS::P, PTS::R, PTS::PRDHS, D> {
+                self.inner
+            }
+            $method_vis fn plan_data_moved(self) -> (PTS::P, D) {
+                PTS::PRDHS::plan_data_moved((self.inner.plan_holder, self.inner.data_holder))
+            }
+            $method_vis fn real_data_moved(self) -> (PTS::R, D) {
+                PTS::PRDHS::real_data_moved((self.inner.real_holder, self.inner.data_holder))
+            }
+            $method_vis fn plan_mut(&mut self) -> &mut PTS::P {
+                self.inner.plan_holder.plan_mut()
+            }
+            $method_vis fn real_mut(&mut self) -> &mut PTS::R {
+                self.inner.real_holder.real_mut()
+            }
+            $method_vis fn data_mut(&mut self) -> &mut D {
+                self.inner.data_holder.data_mut()
+            }
+            /// Whether we are in the `Plan` mode (rather than `Process` mode).
+            $method_vis fn being_planned(&self) -> ::core::primitive::bool {
+                self.inner.plan_holder.being_planned()
+            }
+        }
+
+        /* // Couldn't compile:
+        impl<PTS: $crate::PrdTypes, D: ::core::marker::Send + ::core::marker::Sized, FROM: $crate::MoveInnerExact<PTS, D>> ::core::convert::From<FROM> for Prd<PTS, D> {
+            fn from(value: FROM) -> Self {
+                Self::new(value.move_inner())
+            }
+        }
+        impl<PTS: $crate::PrdTypes, D: ::core::marker::Send + ::core::marker::Sized, FROM: $crate::MoveInner<PrdInner<PTS::P, PTS::R, PTS::PRDHS, D>>> ::core::convert::From<FROM> for Prd<PTS, D> {
+            fn from(value: FROM) -> Self {
+                Self::new(value.move_inner())
+            }
+        }
+        impl<PTS: $crate::PrdTypes, D: ::core::marker::Send + ::core::marker::Sized> $crate::MoveInner<PrdInner<PTS::P, PTS::R, PTS::PRDHS, D>> for Prd<PTS, D> {
+            fn move_inner(self) -> PrdInner<PTS::P, PTS::R, PTS::PRDHS, D> {
+                self.inner
+            }
+        }*/
+        /// For interoperability, so that different crates can convert between their implementations
+        /// of `Prd` (or the struct named as indicated by `$struct_name` param of
+        /// [`distrib::generate_prd_struct`] macro).
+        impl<PTS: $crate::PrdTypes, D: ::core::marker::Send + ::core::marker::Sized> ::core::convert::From<$crate::PrdInner<PTS::P, PTS::R, PTS::PRDHS, D>> for $struct_name<PTS, D> {
+            fn from(value: $crate::PrdInner<PTS::P, PTS::R, PTS::PRDHS, D>) -> Self {
+                Self::new(value)
+            }
+        }
+    };
+    // @TODO Can't match the following with empty (private) last visibility ($method_vis)?!
+    ($struct_vis:vis, $inner_vis:vis, $method_vis:vis) => {
+        $crate::generate_prd_struct!($struct_vis, $inner_vis, $method_vis, Prd);
+    };
+    ($struct_name:ident) => {
+        $crate::generate_prd_struct!(, , , $struct_name);
+    };
+    () => {
+        $crate::generate_prd_struct!(Prd);
     }
 }
 
--- tests/tests.rs
+++ tests/tests.rs
@@ -1,6 +1,9 @@
 use core::marker::PhantomData;
 
-use distrib::{Plan, PlanRealData, PlanRealDataHolders, Prd, Real, TrackT};
+use distrib::{
+    DataHolder, Plan, PlanHolder, PlanRealData, PlanRealDataHolders, PrdInner, PrdTypes, Real,
+    RealHolder,
+};
 
 // #[derive(Track)]
 // #[track_data]
@@ -28,14 +31,14 @@ pub struct Tct<P: Plan, D: Send + Sized> {
 impl<P: Plan, D: Send + Sized> TrackChildT<D> for Tct<P, D> {}
 
 // generated:
-impl<P: Plan, R: Real, D: Send + Sized> TrackT<P, R, D> for Tct<P, D> {
+/*impl<P: Plan, R: Real, D: Send + Sized> TrackT<P, R, D> for Tct<P, D> {
     fn process_t(&self) -> D {
         self.p()
         // OR (if we need to disambiguate):
         //
         // <Self as TrackChildT<T>>::p(self)
     }
-}
+}*/
 
 impl<P: Plan, D: Send + Sized> Tct<P, D> {
     // How do I eliminate `T` data/size from being used in Plan mode, yet have the client code use
@@ -51,11 +54,11 @@ pub struct Processing<P: Plan, R: Real, PRDHS: PlanRealDataHolders<P, R>> {
     _r: PhantomData<R>,
     _prdh: PhantomData<PRDHS>,
 }
+//impl<P: Plan, R: Real, PRDH: PlanRealDataHolders<P, R>> Processing<P, R, PRDH> {}
 
-//impl<P: Plan, R: Real, PRDH: PlanRealDataHolders<P, R>> Processing<P, R, PRDH> {
-pub fn f<P: Plan, R: Real, PRDHS: PlanRealDataHolders<P, R>>(
-    prd: Prd<P, R, PRDHS, &str>,
-) -> Prd<P, R, PRDHS, String> {
+pub fn to_uppercase_f<P: Plan, R: Real, PRDHS: PlanRealDataHolders<P, R>>(
+    prd: PrdInner<P, R, PRDHS, &str>,
+) -> PrdInner<P, R, PRDHS, String> {
     if prd.is_plan() {
         // plan
         let (plan, data) = prd.plan_data_moved();
@@ -67,6 +70,107 @@ pub fn f<P: Plan, R: Real, PRDHS: PlanRealDataHolders<P, R>>(
     loop {}
 }
 
-pub struct S {}
+distrib::generate_prd_struct!(pub, , pub); // OK
+// distrib::generate_prd_struct!(pub, , ); -- NOT OK!
+//distrib::generate_prd_struct!(pub, , , Prd); // OK
+
+pub fn to_uppercase_g<PTS: PrdTypes>(prd: Prd<PTS, &str>) -> Prd<PTS, String> {
+    if prd.being_planned() {
+        // plan -> update/add to Plan
+        let (plan, data) = prd.plan_data_moved();
+        // Copy of the original code, OR adjust manually
+    } else {
+        // execute & collect Real costs
+        let (real, data) = prd.real_data_moved();
+        // Copy of the original code
+    }
+    loop {}
+}
+
+impl<PTS: PrdTypes> Prd<PTS, &str> {
+    pub fn to_uppercase(self) -> Prd<PTS, String> {
+        if self.being_planned() {
+            // plan
+            let (plan, data) = self.plan_data_moved();
+            // Copy of the original code
+        } else {
+            let (real, data) = self.real_data_moved();
+            // Copy of the original code
+        }
+        loop {}
+    }
+}
+
+impl<PTS: PrdTypes> Prd<PTS, String> {
+    pub fn to_uppercase(self) -> Prd<PTS, String> {
+        if self.being_planned() {
+            // plan
+            let (plan, data) = self.plan_data_moved();
+            // Copy of the original code
+        } else {
+            let (real, data) = self.real_data_moved();
+            // Copy of the original code
+        }
+        loop {}
+    }
+}
+
+impl<PTS: PrdTypes> Prd<PTS, Vec<u8>> {
+    pub fn to_word(self) -> Prd<PTS, String> {
+        if self.being_planned() {
+            // plan
+            let (plan, data) = self.plan_data_moved();
+            // Copy of the original code
+        } else {
+            let (real, data) = self.real_data_moved();
+            // Copy of the original code
+        }
+        loop {}
+    }
+}
+
+impl<PTS: PrdTypes> Prd<PTS, Vec<u8>> {
+    pub fn to_word_uppercase(self) -> Prd<PTS, String> {
+        self.to_word().to_uppercase()
+    }
+
+    pub async fn f() {}
+}
+
+pub fn instantiate_outside_struct<PTS: PrdTypes>() {
+    #[allow(unreachable_code)]
+    let _prd = Prd::<PTS, u8>::new(loop {});
+}
+distrib::generate_prd_struct!(pub, , pub, Prd2);
+pub fn move_between_prds<PTS: PrdTypes>() {
+    #[allow(unreachable_code)]
+    let prd = Prd::<PTS, u8>::new(loop {});
+    let prd2: Prd2<PTS, u8> = prd.inner.into();
+    // OR the same the other way, but using .inner() method:
+    let _prd_again: Prd<PTS, u8> = prd2.inner().into();
+}
+
+/// To avoid adding `<PTS: PrdTypes>` generic parameter to every function, we can workaround with a
+/// trait and have that `PTS` as an associated type of that trait.
+///
+/// Any functions are implemented in the trait (as default implementations). Of course, you can add
+/// generic parameters to the trait, as needed.
+///
+/// But, this doesn't save much. Plus, invoking these functions is *less* ergonomic.
+pub trait T {
+    type PTS: PrdTypes;
+
+    fn f(prd: Prd<Self::PTS, &str>) -> Prd<Self::PTS, String> {
+        if prd.being_planned() {
+            // plan
+            let (plan, data) = prd.plan_data_moved();
+            // Copy of the original code
+        } else {
+            let (real, data) = prd.real_data_moved();
+            // Copy of the original code
+        }
+        loop {}
+    }
+}
 
 //impl <P: Plan, R: Real, PRDH: PlanRealDataHolders<P, R>> Prd<P, R, PRDH, S> {}
